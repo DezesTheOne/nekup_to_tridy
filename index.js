b61050286c37adac54592d2b26d2c4dc9bdfc7ae
Hedvika Reichlová <143612786+DezesTheOne@users.noreply.github.com>
2023-09-23 13:44:39 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const product1 = {
+  image: 'images/destniklobouk.jpg',
+  name: 'Deštníklobouk',
+  description: 'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
+};
+
+const product2 = {
+  image: 'images/slepickabelka.jpg',
+  name: 'Slepičkabelka',
+  description: 'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
+};
+
+const product3 = {
+  image: 'images/anatoplavky.jpg',
+  name: 'Anatoplavky',
+  description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
+};
